65106c4f40e4cec307301073c7c1f8ae7d828500
Iceflue2 <svyatoslavsvyatkin@yandex.ru>
2023-07-02 16:06:09 +0300

    fix bug with query parameters at /bookings/query

--- src/main/kotlin/com/paranid5/innobookingfakeapi/data/BookQueryRequest.kt
+++ src/main/kotlin/com/paranid5/innobookingfakeapi/data/BookQueryRequest.kt
@@ -9,9 +9,9 @@ data class BookQueryRequest(val filter: Filter) {
     @Serializable
     data class Filter(
         @SerialName("started_at_or_after") val startedAtOrAfter: Instant,
-        @SerialName("ended_at_or_after") val endedAtOrAfter: Instant,
+        @SerialName("ended_at_or_before") val endedAtOrAfter: Instant,
+        @SerialName("owner_email_in") val ownerEmailIn: List<String> = listOf(),
         @SerialName("room_id_in") val roomIdIn: List<String> = listOf(),
-        @SerialName("owner_email_in") val ownerEmailIn: List<String> = listOf()
     )
 
     constructor(
